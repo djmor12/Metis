eadde36fa42116fd3818910390a3030cf8de1824
Deven Morris <djmor12.deven@gmail.com>
2018-05-16 06:42:13 -0700

    Not quite the final clean
    made some adjustments, but not quite the final cleanup

---
+++ Talking Data/Flask/static/mod_dd.js
@@ -0,0 +1,9 @@
+var d_dd = [
+  {value: 6, text: 'Sunday'},
+  {value: 0, text: 'Monday'},
+  {value: 1, text: 'Tuesday'},
+  {value: 2, text: 'Wednesday'},
+  {value: 3, text: 'Thursday'},
+  {value: 4, text: 'Friday'},
+  {value: 5, text: 'Saturday'}
+]
