e9c581f7e09485fcde5a72702b05909bf74e7893
Deven Morris <djmor12.deven@gmail.com>
2018-05-28 16:27:25 -0700

    changed a few things..

---
+++ ProjectFletcher /Flask/static/mod_dd.js
@@ -0,0 +1,9 @@
+var d_dd = [
+  {value: 'Green', text: 'Green'},
+  {value: 'Black', text: 'Black'},
+  {value: 'Yellow', text: 'Yellow'},
+  {value: 'White', text: 'White'},
+  {value: 'Pu-erh', text: 'Pu-erh'},
+  {value: 'Matcha', text: 'Matcha'},
+  {value: 'Oolong', text: 'Oolong'}
+]
